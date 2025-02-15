620f6f3143d97ac5a3215b366778a7a1a473d33e
vineela <vineelabodapati4@gmail.com>
2025-02-15 19:17:56 +0530

    footer is created with common components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,46 @@
-// import React from "react";
-// import "./footer.css";
+import React from "react";
+//import "./Footer.css";  // Ensure this path is correct
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Hobby Travel Matchmaker. All rights reserved.</p>
+      <div className="footer-container">
+        <div className="footer-section">
+          <h4>About Us</h4>
+          <ul>
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <h4>Support</h4>
+          <ul>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Help Center</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <h4>Follow Us</h4>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Twitter</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section newsletter">
+          <h4>Subscribe to Our Newsletter</h4>
+          <form>
+            <input type="email" placeholder="Enter your email" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
+      </div>
+      <p className="footer-bottom">© 2025 RouteCrafters. All rights reserved.</p>
     </footer>
   );
 };
